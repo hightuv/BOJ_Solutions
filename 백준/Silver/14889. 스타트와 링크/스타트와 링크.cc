b65a98200fc1b3c3048eb177036c0b88345353f4
Minseo Park <hightuv@gmail.com>
2023-04-08 14:39:04 +0900

    [Silver II] Title: 스타트와 링크, Time: 124 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <climits>
+#define MAX 21
+using namespace std;
+
+int N;
+
+int S[MAX][MAX];
+
+bool visited[MAX];
+
+int minimum = INT_MAX;
+
+void dfs(int count, int next) {
+    if (count == N / 2) {
+
+        int startTotal = 0;
+        int linkTotal = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (visited[i] == true && visited[j] == true) {
+                    startTotal += S[i][j];
+                }
+                else if (visited[i] == false && visited[j] == false) {
+                    linkTotal += S[i][j];
+                }
+            }
+        }
+
+        int diff = abs(startTotal - linkTotal);
+
+        if (minimum > diff) {
+            minimum = diff;
+        }
+
+        return;
+    }
+
+    for (int i = next; i <= N; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            dfs(count + 1, i + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> S[i][j];
+        }
+    }
+
+    dfs(0, 1);
+
+    cout << minimum << '\n';
+}
