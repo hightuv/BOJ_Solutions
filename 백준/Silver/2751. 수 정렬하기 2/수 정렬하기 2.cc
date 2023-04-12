243c034c9632a61198b6a8046b7dba3470e91863
Minseo Park <hightuv@gmail.com>
2023-04-12 23:21:37 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 272 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1000001
+using namespace std;
+
+int arr[MAX];
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr + n);
+    
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << '\n';
+    }
+}
